3c423ba9a46b3bed6bb91ad49c2578cb4d195925
Georges Haddad <georges7haddad@gmail.com>
2021-03-20 00:05:07 +0200

    [Add] User login [Add] User Sign Up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import "./App.css";
+import UserCredentialsDialog from "./UserCredentialsDialog/UserCredentialsDialog";
 import { useState } from "react";
 import { useEffect } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Snackbar,
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import { getUserToken, saveUserToken } from "./localStorage";
 
 var SERVER_URL = "http://127.0.0.1:5000";
 function App() {
@@ -9,7 +19,14 @@ function App() {
   let [lbpInput, setLbpInput] = useState("");
   let [usdInput, setUsdInput] = useState("");
   let [transactionType, setTransactionType] = useState("usd-to-lbp");
-
+  let [userToken, setUserToken] = useState(getUserToken());
+  const States = {
+    PENDING: "PENDING",
+    USER_CREATION: "USER_CREATION",
+    USER_LOG_IN: "USER_LOG_IN",
+    USER_AUTHENTICATED: "USER_AUTHENTICATED",
+  };
+  let [authState, setAuthState] = useState(States.PENDING);
   function fetchRates() {
     fetch(`${SERVER_URL}/exchangeRate`)
       .then((response) => response.json())
@@ -35,12 +52,96 @@ function App() {
     }).then(fetchRates);
   }
 
+  function login(username, password) {
+    return fetch(`${SERVER_URL}/authentication`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_name: username,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((body) => {
+        setAuthState(States.USER_AUTHENTICATED);
+        setUserToken(body.token);
+        saveUserToken(body.token);
+      });
+  }
+
+  function createUser(username, password) {
+    return fetch(`${SERVER_URL}/user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_name: username,
+        password: password,
+      }),
+    }).then((response) => login(username, password));
+  }
+
+  function logout() {
+    setUserToken(null);
+    localStorage.removeItem("TOKEN");
+  }
+
   return (
     <div>
       <div>
-        <h1 className="header">Lebanse Pound Exchange Rate Tracker</h1>
+        <AppBar position="static">
+          <Toolbar classes={{ root: "nav" }}>
+            <Typography variant="h5">Lebanese Rate Exchange</Typography>
+            {userToken !== null ? (
+              <Button color="inherit" onClick={logout}>
+                Logout
+              </Button>
+            ) : (
+              <div>
+                <Button
+                  color="inherit"
+                  onClick={() => setAuthState(States.USER_CREATION)}
+                >
+                  Register
+                </Button>
+                <Button
+                  color="inherit"
+                  onClick={() => setAuthState(States.USER_LOG_IN)}
+                >
+                  Login
+                </Button>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
       </div>
       <div className="wrapper">
+        <Snackbar
+          elevation={6}
+          variant="filled"
+          open={authState === States.USER_AUTHENTICATED}
+          autoHideDuration={2000}
+          onClose={() => setAuthState(States.PENDING)}
+        >
+          <Alert severity="success">Success</Alert>
+        </Snackbar>
+        <UserCredentialsDialog
+          open={authState === States.USER_CREATION ? true : false}
+          onSubmit={createUser}
+          onClose={() => setAuthState(States.PENDING)}
+          title="Register"
+          submitText="Sign Up"
+        />
+        <UserCredentialsDialog
+          open={authState === States.USER_LOG_IN ? true : false}
+          onSubmit={login}
+          onClose={() => setAuthState(States.PENDING)}
+          title="Login"
+          submitText="Login"
+        />
         <h2>Today's Exchange Rate</h2>
         <p>LBP to USD Exchange Rate</p>
         <h3>
